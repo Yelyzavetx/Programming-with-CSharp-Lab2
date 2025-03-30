ad195954ead21582847d064b6b52687ad200561e
Yelyzaveta Petrenko <elispetrenko23@gmail.com>
2025-03-30 04:51:24 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab2;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        FirstNameBox.TextChanged += ValidateInput;
+        LastNameBox.TextChanged += ValidateInput;
+        EmailBox.TextChanged += ValidateInput;
+        BirthDatePicker.SelectedDateChanged += ValidateInput;
+    }
+
+    private void ValidateInput(object sender, EventArgs e)
+    {
+        ProceedButton.IsEnabled =
+            !string.IsNullOrWhiteSpace(FirstNameBox.Text) &&
+            !string.IsNullOrWhiteSpace(LastNameBox.Text) &&
+            !string.IsNullOrWhiteSpace(EmailBox.Text) &&
+            BirthDatePicker.SelectedDate.HasValue;
+    }
+
+    private async void ProceedButton_Click(object sender, RoutedEventArgs e)
+    {
+        ProceedButton.IsEnabled = false;
+
+        string firstName = FirstNameBox.Text;
+        string lastName = LastNameBox.Text;
+        string email = EmailBox.Text;
+        DateTime birthDate = BirthDatePicker.SelectedDate.Value;
+
+        var person = new Person(firstName, lastName, email, birthDate);
+
+        TextBlockFisrtName.Text = person.FirstName;
+        TextBlockLastName.Text = person.LastName;
+        TextBlockEmail.Text = person.Email;
+        TextBlockBirthdayDate.Text = person.BirthDate.ToString("dd.MM.yyyy");
+        TextBlockIsAdult.Text = person.IsAdult ? "Yes" : "No";
+        TextBlockChineseZodiac.Text = person.ChineseSign;
+        TextBlockSunSign.Text = person.SunSign;
+        TextBlockIsBirthdayToday.Text = person.IsBirthday ? "Yes" : "No";
+        if (birthDate.Day == DateTime.Today.Day && birthDate.Month == DateTime.Today.Month)
+        {
+            TextBlockWishes.Text = "Happy birthday!";
+        }
+        else
+        {
+            TextBlockWishes.Text = "";
+        }
+    }
+    private void ExitApplication(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
+       
---
+++ Person.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Xml.Linq;
+
+namespace Lab2
+{
+    class Person
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private DateTime _birthDate;
+
+        //Public properties for accessing private fields
+        public string FirstName => _firstName;
+        public string LastName => _lastName;
+        public string Email => _email;
+        public DateTime BirthDate => _birthDate;
+
+
+        public bool IsAdult => CalculateIsAdult();
+        public bool IsBirthday => CalculateIsBirthday();
+        public string SunSign => GetWesternZodiacSign();
+        public string ChineseSign => GetChineseZodiacSign();
+
+
+        // Constructor with 4 parameters
+        public Person(string firstName, string lastName, string email, DateTime birthDate)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _email = email;
+            _birthDate = birthDate;
+        }
+
+        // Constructor without email
+        public Person(string firstName, string lastName, DateTime birthDate)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _email = string.Empty;
+            _birthDate = birthDate;
+        }
+
+
+        // Constructor without birthDate
+        public Person(string firstName, string lastName, string email)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _email = email;
+            _birthDate = DateTime.MinValue;
+        }
+
+
+
+        private bool CalculateIsAdult()
+        {
+            if (DateTime.Today < _birthDate)
+            {
+                MessageBox.Show("Invalid birth date.");
+                return false;
+            }
+
+            int age = DateTime.Today.Year - _birthDate.Year - (DateTime.Today.DayOfYear < _birthDate.DayOfYear ? 1 : 0);
+
+            if (age > 135)
+            {
+                MessageBox.Show("The person is over 135 years old. Invalid age.");
+                return false;
+            }
+
+            return age >= 18;
+        }
+
+
+        private bool CalculateIsBirthday()
+        {
+            return _birthDate.Day == DateTime.Today.Day && _birthDate.Month == DateTime.Today.Month;
+        }
+
+        private string GetWesternZodiacSign() //zodiak sign
+        {
+            int month = _birthDate.Month;
+            int day = _birthDate.Day;
+            if ((month == 3 && day >= 21) || (month == 4 && day <= 19)) return "Aries";
+            if ((month == 4 && day >= 20) || (month == 5 && day <= 20)) return "Taurus";
+            if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) return "Gemini";
+            if ((month == 6 && day >= 21) || (month == 7 && day <= 22)) return "Cancer";
+            if ((month == 7 && day >= 23) || (month == 8 && day <= 22)) return "Leo";
+            if ((month == 8 && day >= 23) || (month == 9 && day <= 22)) return "Virgo";
+            if ((month == 9 && day >= 23) || (month == 10 && day <= 22)) return "Libra";
+            if ((month == 10 && day >= 23) || (month == 11 && day <= 21)) return "Scorpio";
+            if ((month == 11 && day >= 22) || (month == 12 && day <= 21)) return "Sagittarius";
+            if ((month == 12 && day >= 22) || (month == 1 && day <= 19)) return "Capricorn";
+            if ((month == 1 && day >= 20) || (month == 2 && day <= 18)) return "Aquarius";
+            return "Pisces";
+        }
+
+        private string GetChineseZodiacSign() //chinese zodiak sign
+        {
+            int year = _birthDate.Year;
+            string[] chineseZodiacs = { "Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat" };
+            return chineseZodiacs[year % 12];
+        }
+    }
+}
